f0ac07f3f9df52158714a7386c029a1a664c85f9
Wswsvic <166273629+Wswsvic@users.noreply.github.com>
2026-04-14 16:25:52 +0800

    W1: fix gitignore to exclude docker data volume

---
+++ src/test/java/org/wsws/apartment/SmartApartmentAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wsws.apartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartApartmentAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
